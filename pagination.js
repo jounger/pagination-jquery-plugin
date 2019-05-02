b920b118edad9e11ac6233b24e7efa89a2e71193
Nguyen Van An <44369558+jounger@users.noreply.github.com>
2019-05-02 16:58:52 +0700

    Update new Boundary option
    boundary default is false, you can change it to show "first and last" option in paging

--- pagination.js
+++ pagination.js
@@ -2,19 +2,22 @@
  	$.fn.drawPage = function( options ) {
  		var settings = $.extend({
             from: 1,
-            to: 1,
+			to: 1,
+			boundary: false,
         }, options );
         return this.each(function(){
         	var $this = $(this).children('ul');
 	        $this.empty();
 	        var $preTag = '<li class="page-item"><a class="page-link" href="javascript:void(0)" tabindex="-1"';
-	        var $endTag = '</a></li>';
-			$this.append($preTag + 'id="page-prev" data-index="0">Previous' + $endTag);
+			var $endTag = '</a></li>';
+			if(settings.boundary == true) $this.append($preTag + 'id="page-first" data-index="0">First' + $endTag);
+			$this.append($preTag + 'id="page-previous" data-index="0">Previous' + $endTag);
 			var $index = 0;
 			for (var i = settings.from; i <= settings.to; i++) {
 				$this.append($preTag + 'data-index="'+(++$index)+'">'+ i + $endTag);
 			}
 			$this.append($preTag + 'id="page-next" data-index="'+(++$index)+'">Next' + $endTag);
+			if(settings.boundary == true) $this.append($preTag + 'id="page-last" data-index="0">Last' + $endTag);
         });
  	}
 	 $.fn.pageChanging = function( options, callback ) {
@@ -22,34 +25,39 @@
             size: 1,
             pageShow: 5,
             page: 1,
-            limit: 1,
+			limit: 1,
+			boundary: false,
         }, options );
         return this.each(function(){
-        	var $parentTag = $(this);
+			var $parentTag = $(this);
         	$(this).children('ul').click(
 				function(event) {
+					var $isBoundary = settings.boundary===true?1:0;
 					var $thisChildren = $(this).children('li');
 					var $position = $(event.target).attr('data-index');
 					var $totalPage = Math.ceil(settings.size / settings.limit);
 					var $index = parseInt($($thisChildren.filter('.active').html()).attr('data-index'));
-					var $firstPositon = parseInt($($thisChildren.eq(1).html()).html());
-					var $lastPositon = parseInt($($thisChildren.eq($thisChildren.length - 2).html()).html());
+					var $firstPositon = parseInt($($thisChildren.eq(1 + $isBoundary).html()).html());
+					var $lastPositon = parseInt($($thisChildren.eq($thisChildren.length - (2 + $isBoundary)).html()).html());
 					var $active = $($thisChildren.filter('.active').html()).html();
-					var $page = parseInt($active);
+					var $page = isNaN($active)?1:parseInt($active);
 					var $drawMain = false;
-					switch ($(event.target).html().toLowerCase()) {
+					var $currentPage = $(event.target).html().toLowerCase();
+					switch ($currentPage) {
 					case 'first':
 						if ($page > 1) {
 							$page = 1;
 							$drawMain = true;
+							$position = 1;
 						} else {
 							return true;
 						}
 						break;
 					case 'last':
-						if ($page * $limit < $totalPage) {
+						if (settings.page * settings.limit < $totalPage) {
 							$page = $totalPage;
 							$drawMain = true;
+							$position = settings.pageShow;
 						} else {
 							return true;
 						}
@@ -80,26 +88,36 @@
 							return true;
 						}
 					}
+					var _from = 1, _to = settings.pageShow;
+					if($currentPage === 'first') {
+						_from = 1;
+						_to = $totalPage>settings.pageShow?settings.pageShow:$totalPage;
+					} else if($currentPage === 'last') {
+						_from = $totalPage - settings.pageShow + 1;
+						_to = $totalPage;
+					} else if ($position >= settings.pageShow && $lastPositon < $totalPage) {
+						_from = $lastPositon - (settings.pageShow - 2);
+						_to = $lastPositon + 1;
+						$position = settings.pageShow - 1;
+					} else if ($position <= 1 && $firstPositon > 1) {
+						_from = $firstPositon - 1;
+						_to = $firstPositon + (settings.pageShow - 2);
+						$position = 2;
+					} else {
+						_from = $firstPositon;
+						_to = $lastPositon;
+					}
 					if($drawMain && $.isFunction(callback) && ($(event.target).html() !== $active)) {
 						callback.call(this, {size: settings.size, page : $page, limit: settings.limit});
-					}
-					if ($position >= settings.pageShow && $lastPositon < $totalPage) {
 						$parentTag.drawPage({
-							from: $lastPositon - (settings.pageShow - 2),
-							to: $lastPositon + 1,
+							from: _from,
+							to: _to,
+							boundary: settings.boundary,
 						});
-						$position = settings.pageShow - 1;
-					}
-					if ($position <= 1 && $firstPositon > 1) {
-						$parentTag.drawPage({
-							from: $firstPositon - 1,
-							to: $firstPositon + (settings.pageShow - 2),
-						});
-						$position = 2;
 					}
 					if($position > 0 && $position < ($totalPage>settings.pageShow?(settings.pageShow+1):$totalPage+1) && $totalPage > 1) {
-						$parentTag.find('ul > li').eq($position).addClass('active').siblings().removeClass('active');
-					}	
+						$parentTag.find('ul > li').eq(parseInt($position)+$isBoundary).addClass('active').siblings().removeClass('active');
+					}
 			});
         });
  	}
@@ -108,7 +126,8 @@
             size: 1,
             pageShow: 5,
             page: 1,
-            limit: 1,
+			limit: 10,
+			boundary : false,
         }, options );
 		if(settings.pageShow < 3) settings.pageShow = 3;
         return this.each( function() {
@@ -119,15 +138,17 @@
         	$(this).append('<ul class="pagination"></ul>');
         	$(this).drawPage({
         		from: 1,
-        		to: $toFirstTime,
+				to: $toFirstTime,
+				boundary: settings.boundary,
         	});
-			$(this).find('ul > li').eq(1).addClass('active');
+			$(this).find('ul > li').eq(settings.boundary===true?2:1).addClass('active');
 			callback.call(this, {page : 1});
             $(this).pageChanging({
 	            size: settings.size,
 	            pageShow: settings.pageShow,
 	            page: settings.page,
-	            limit: settings.limit,
+				limit: settings.limit,
+				boundary : settings.boundary
         	}, callback);
         });
  
